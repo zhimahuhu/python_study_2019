0ddda63fcc4430cfe5353b97e6aa35ae1933f58f
zhimahuhu <loluka@sina.com>
2019-12-16 22:30:36 +0800

    Django

---
+++ learning_log/learning_logs/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+app_name = 'learning_logs'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^topics$', views.topics, name='topics'),
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+]
